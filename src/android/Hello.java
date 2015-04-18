7368192e3cdf162544259e917a71b870b2af315a
Vimal Bhalodia <vimal@alum.mit.edu>
2015-04-18 13:36:01 -0700

    testing

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -4,12 +4,36 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.*;
+import android.content.*;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.location.GpsStatus.NmeaListener;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.*;
+import android.util.Log;
+
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        //if (action.equals("greet")) {
 
             String name = data.getString(0);
             String message = "Hello, " + name;
